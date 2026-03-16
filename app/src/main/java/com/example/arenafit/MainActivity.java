ed14d4a344249b16711ae2f3ab09139cbfed7b61
tamirDemri <tamirdemri@gmail.com>
2026-03-16 16:54:51 +0200

    pushing the beta window

--- app/src/main/java/com/example/arenafit/MainActivity.java
+++ app/src/main/java/com/example/arenafit/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.arenafit;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private EditText username;
+    private EditText password;
+
+    private Button signin;
+    private Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        EdgeToEdge.enable(this);
+
+        username = findViewById(R.id.userName); // linking between var and visual
+        password = findViewById(R.id.password);
+        signin = findViewById(R.id.sign_in_button);
+        signup = findViewById(R.id.sign_up_button);
+
+        signin.setOnClickListener(this); // for the button to listen
+        signup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if(view == signin)
+        {
+            //sign in
+        }
+        else if(view == signup)
+        {
+            //sign up
+        }
     }
 }
